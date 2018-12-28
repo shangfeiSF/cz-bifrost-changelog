9dc9e9a0218e0ff2ba5ef96e74589e896a918d9a
shangfei <shangfei@baidu.com>
2018-12-28 09:49:49 +0800

    add the new config named 29_2

--- src/scopes/index.js
+++ src/scopes/index.js
@@ -3,5 +3,6 @@ module.exports = {
     '25-30_1': require('./modules/25-30_1'),
     '2_2': require('./modules/2_2'),
     '8_2': require('./modules/8_2'),
+    '29_2': require('./modules/29_2'),
     'wind': require('./modules/wind')
 };
